8990d67640fe5d07df9be7229b823c81a062f027
Haseena Sainul <h.sainul@metrological.com>
2022-10-07 04:49:27 -0700

    IDeviceInfo: inprogress version

--- definitions/Definitions.cpp
+++ definitions/Definitions.cpp
@@ -90,32 +90,18 @@ struct ScreenResolutionWidthHeight {
 
 ScreenResolutionWidthHeight resolutionWidthHeightTable[] = {
 
-    { Exchange::IComposition::ScreenResolution_Unknown, 0, 0 },
-    { Exchange::IComposition::ScreenResolution_480i, 640, 480 },
-    { Exchange::IComposition::ScreenResolution_480p, 640, 480 },
-    { Exchange::IComposition::ScreenResolution_720p, 1280, 720 },
-    { Exchange::IComposition::ScreenResolution_720p50Hz, 1280, 720 },
-    { Exchange::IComposition::ScreenResolution_1080p24Hz, 1920, 1080 },
-    { Exchange::IComposition::ScreenResolution_1080i50Hz, 1920, 1080 },
-    { Exchange::IComposition::ScreenResolution_1080p50Hz, 1920, 1080 },
-    { Exchange::IComposition::ScreenResolution_1080p60Hz, 1920, 1080 },
-    { Exchange::IComposition::ScreenResolution_2160p50Hz, 3840, 2160 },
-    { Exchange::IComposition::ScreenResolution_2160p60Hz, 3840, 2160 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_Unknown, 0, 0 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_480i, 640, 480 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_480p, 640, 480 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_720p, 1280, 720 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_720p50Hz, 1280, 720 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_1080p24Hz, 1920, 1080 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_1080i50Hz, 1920, 1080 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_1080p50Hz, 1920, 1080 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_1080p60Hz, 1920, 1080 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_2160p50Hz, 3840, 2160 },
+    { Exchange::IComposition::ScreenResolution::ScreenResolution_2160p60Hz, 3840, 2160 },
 };
-
-ENUM_CONVERSION_BEGIN(Exchange::IComposition::ScreenResolution)
-    { Exchange::IComposition::ScreenResolution_Unknown, _TXT("Unknown") },
-    { Exchange::IComposition::ScreenResolution_480i, _TXT("480i") },
-    { Exchange::IComposition::ScreenResolution_480p, _TXT("480p") },
-    { Exchange::IComposition::ScreenResolution_720p, _TXT("720p") },
-    { Exchange::IComposition::ScreenResolution_720p50Hz, _TXT("720p50Hz") },
-    { Exchange::IComposition::ScreenResolution_1080p24Hz, _TXT("1080p24Hz") },
-    { Exchange::IComposition::ScreenResolution_1080i50Hz, _TXT("1080i50Hz") },
-    { Exchange::IComposition::ScreenResolution_1080p50Hz, _TXT("1080p50Hz") },
-    { Exchange::IComposition::ScreenResolution_1080p60Hz, _TXT("1080p60Hz") },
-    { Exchange::IComposition::ScreenResolution_2160p50Hz, _TXT("2160p50Hz") },
-    { Exchange::IComposition::ScreenResolution_2160p60Hz, _TXT("2160p60Hz") },
-ENUM_CONVERSION_END(Exchange::IComposition::ScreenResolution)
 
 ENUM_CONVERSION_BEGIN(Exchange::IStream::streamtype)
     { Exchange::IStream::streamtype::Undefined, _TXT(_T("Undefined")) },
